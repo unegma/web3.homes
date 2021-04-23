29a756a41959065534b2e48bb3cbdc9f89670e6c
Timothy Coleman <timhc22@gmail.com>
2021-04-23 14:09:03 +0100

    Add about and affiliate links

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Container} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 
 export default function About(): JSX.Element {
 
@@ -7,7 +8,18 @@ export default function About(): JSX.Element {
     <section className="container">
       <div>
         <Container maxWidth="sm">
-          Hi
+          <Typography paragraph className="pageText--heading">
+            About
+          </Typography>
+          <Typography className="pageText--body">This site is meant as an easy interface for corporations and business users to submit proposals for improvements to the <a href="https://ethereum.org/" target="_blank">Ethereum protocol</a>.</Typography>
+          <br/>
+          <Typography className="pageText--body">Each proposal will be reviewed, and then submitted to the official Ethereum team in the <a href="https://eips.ethereum.org/EIPS/eip-1" target="_blank">accepted technical format.</a></Typography>
+          <br/>
+          <Typography className="pageText--body">This site is by <a href="https://unegma.com" target="_blank">Unegma LTD</a> who are not currently affiliated directly with the Ethereum Foundation.</Typography>
+          <br/>
+          <Typography className="pageText--body">For the purposes of this site, <a href="https://unegma.com" target="_blank">Unegma LTD</a> is currently affiliated with the following organisations:</Typography>
+          <br/>
+          <a href="https://ethldn.org" target="_blank"><img className="affiliate--ethldn" src="https://ethldn.org/wp-content/uploads/2019/05/unicorn-image-w-stars.png"></img></a>
 
         </Container>
       </div>
