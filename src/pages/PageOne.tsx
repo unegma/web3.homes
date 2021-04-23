19ea54481f27b9ad43e0971123c0a7ea02fe7fb2
Timothy Coleman <timhc22@gmail.com>
2021-04-23 15:08:58 +0100

    Add form and styling

--- src/pages/PageOne.tsx
+++ src/pages/PageOne.tsx
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from 'react';
 import {Container} from "@material-ui/core";
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
+import { TextField } from "@material-ui/core";
+import {Button, Input} from "@material-ui/core";
+
 
 export default function PageOne(): JSX.Element {
 
@@ -34,17 +37,65 @@ export default function PageOne(): JSX.Element {
     <section className="container">
       <div>
         <Container maxWidth="sm">
-          Hi
-
-          {/*<img src={imageUrl} />*/}
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register('firstName')} /> {/* register an input */}
-            <input {...register('lastName', { required: true })} />
-            {errors.lastName && <p>Last name is required.</p>}
-            <input {...register('age', { pattern: /\d+/ })} />
-            {errors.age && <p>Please enter number for age.</p>}
-            <input type="submit" />
+            <TextField
+              variant="outlined"
+              {...register('name')}
+              label="Full Name"
+            />
+            <br/>
+
+            <TextField
+              variant="outlined"
+              {...register('email', { required: true })}
+              label="Email"
+            />
+            {errors.email && <span className="error"><br/>An email is required.</span>}
+            <br/>
+
+            <TextField
+              variant="outlined"
+              {...register('organisation', { required: true })}
+              label="Organisation/Organization"
+            />
+            {errors.organisation && <span className="error"><br/>An organisation is required.</span>}
+            <br/>
+
+            <br/>
+
+            <TextField
+              multiline
+              variant="outlined"
+              rows={2}
+              {...register('why', { required: true }) } label="The 'Why'"
+            />
+            {errors.why && <span className="error"><br/>Why is this needed?</span>}
+            <br/>
+
+            <TextField
+              multiline
+              variant="outlined"
+              rows={2}
+              {...register('summary', { required: true })} label="A Simple Summary"
+            />
+            {errors.summary && <span className="error"><br/>Please give a very simple summary (like you would in the 'subject' of an email).</span>}
+            <br/>
+
+            <TextField
+              multiline
+              variant="outlined"
+              rows={4}
+              {...register('info', { required: true })} label="Details"
+            />
+            {errors.info && <span className="error"><br/>Details required (don't worry, we will get in touch if anything is missing).</span>}
+            <br/>
+
+            <br/>
+
+            <Button type="submit" color="primary" variant="contained" className="list-item">
+              Submit
+            </Button>
           </form>
 
         </Container>
