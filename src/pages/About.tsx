534a0dc91d9ef6c4f74767f7d1f722a25e336682
Timothy Coleman <timhc22@gmail.com>
2021-04-23 15:23:46 +0100

    Add contact details re site to about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ export default function About(): JSX.Element {
           <br/>
           <Typography className="pageText--body">Each proposal will be reviewed, and then submitted to the official Ethereum team in the <a href="https://eips.ethereum.org/EIPS/eip-1" target="_blank">accepted technical format.</a></Typography>
           <br/>
-          <Typography className="pageText--body">This site is by <a href="https://unegma.com" target="_blank">Unegma LTD</a> who are not currently affiliated directly with the Ethereum Foundation.</Typography>
+          <Typography className="pageText--body">This site is by <a href="https://unegma.com" target="_blank">Unegma LTD</a> who are not currently affiliated directly with the Ethereum Foundation. For suggestions re this site, please <a href="https://unegma.com/contact" target="_blank">let us know here</a>.</Typography>
           <br/>
           <Typography className="pageText--body">For the purposes of this site, <a href="https://unegma.com" target="_blank">Unegma LTD</a> is currently affiliated with the following organisations:</Typography>
           <br/>
