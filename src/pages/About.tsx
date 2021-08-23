9641382d6f152e9c472355ad5b1ca7073fd86a10
Timothy Coleman <timhc22@gmail.com>
2021-08-23 19:05:18 +0100

    Add to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -11,9 +11,16 @@ export default function About(): JSX.Element {
           <Typography paragraph className="pageText--heading">
             About
           </Typography>
-          <Typography paragraph className="">
-            Built by <a target="_blank" href="https://unegma.com">Unegma</a>
-          </Typography>
+          {/*<Typography paragraph className="">*/}
+          {/*  Built by <a target="_blank" href="https://unegma.com">Unegma</a>*/}
+          {/*</Typography>*/}
+
+          <Typography className="pageText--body">This site is by <a href="https://unegma.com" target="_blank">Unegma LTD</a> who are not currently affiliated directly with ENS Domains or Unstoppable Domains. For suggestions re this site: <a href="https://github.com/unegma/web3.homes" target="_blank">see here</a>.</Typography>
+          <br/>
+          <Typography className="pageText--body">For the purposes of this site, <a href="https://unegma.com" target="_blank">Unegma LTD</a> is currently affiliated with the following organisations:</Typography>
+          <br/>
+          <a href="https://ethldn.org" target="_blank"><img className="affiliate--ethldn" src="https://ethldn.org/wp-content/uploads/2019/05/unicorn-image-w-stars.png"></img></a>
+
         </Container>
       </div>
     </section>
