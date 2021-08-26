7ea8f26c23e6037e6db49921099135f7808012d2
Timothy Coleman <timhc22@gmail.com>
2021-08-26 15:47:25 +0100

    Update language

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,23 +19,22 @@ export default function Home(): JSX.Element {
           <i>(This site is currently a POC/MVP)</i><br/><br/>
         </Typography>
         {/*<Typography className="">Your one stop shop for on-boarding into the Web3 ecosystem.</Typography>*/}
-        <Typography className="">When navigating around the internet, <br/>Do most of us remember and use:&nbsp;
-          <a href="http://172.217.169.78" target="_blank">172.217.169.78</a><br/>
-          Or human rememberable words: <a href="https://google.com" target="_blank">google.com</a>?
+        <Typography className="">
+          When you want a parcel delivered, you use:<br/>
+          Your address, not your GPS co-ordinates.
           <br/><br/>
         </Typography>
         <Typography className="">
-          <b>Why should this be any different in Web3.</b><br/><br/>
+          <b>It should be like this on the blockchain too!</b><br/><br/>
         </Typography>
         <Typography noWrap>
-
-          Would you prefer to say:&nbsp;<br/>
-          Please send 1 ETH to: "<a href="#">0xEF9D542Cd93c6300b5BB755dff4033Eb0c8f8e01</a>"<br/>
-          Or "Please send to: <a href="#">unegma.eth</a>"?<br/><br/>
+          Let's send 1ETH to: <a href="#">unegma.eth</a><br/>
+          (Not to: <a href="#">0xEF9D542Cd93c6300b5BB755dff4033Eb0c8f8e01</a>)
         </Typography>
         <Typography>
-          <sub>(Take care when using your domain name with some services such as exchanges).</sub>
+          <sub>(Take care when using your web3 home with some services such as exchanges).</sub>
         </Typography>
+        <br/>
         <br/>
         <Button variant="contained" color="primary" href="/request">
           Get Started
